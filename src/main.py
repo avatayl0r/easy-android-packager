929f4580f0c897f0ff1255db853dc45c099b8978
Aryn Taylor <aryntaylor2021@gmail.com>
2023-03-03 21:58:26 +0000

    Added Directory Finder, psBuilder, colorModes, & base cTK layout

--- src/main.py
+++ src/main.py
@@ -1,27 +1,68 @@
+import subprocess as sp
+import customtkinter as ctk
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
+from tkinter import filedialog as fg
+from PIL import Image
 
-class GUI:
+currentMode = False
+buildDir = ''
+fgImage = ctk.CTkImage(dark_image=Image.open(r"assets\icon_file.png"))
+
+class Main(ctk.CTk):
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Easy Android Packager")
-        self.window.resizable(width=False, height=False)
-        self.window.geometry("350x150")
+        super().__init__()
+        self.title('Easy Android Packager')
+        self.geometry('300x100')
+        self.resizable(width='false', height='false')
 
-        self.label = tk.Label(self.window, text="Tester", font=('Roboto', 18))
-        self.label.pack(padx=20, pady=10)
+        ctk.set_appearance_mode('dark')
 
-        self.filePathFrame = tk.Frame(self.window)
-        self.filePathFrame.columnconfigure(0, weight=1)
-        self.filePathFrame.columnconfigure(1, weight=1)
+        self.colorModeButton = ctk.CTkButton(
+            self,
+            text = 'Color Mode',
+            command = self.toggleColorMode
+            )
+        self.colorModeButton.pack()
 
-        self.textFilePath = tk.Entry(self.filePathFrame, font=('Roboto', 14))
-        self.textFilePath.grid(row=0, column=0, sticky=tk.W + tk.E)
+        self.askDirButton = ctk.CTkButton(
+            self,
+            width=32,
+            height=32,
+            text='',
+            image=fgImage,
+            command=self.askDir
+        )
+        self.askDirButton.pack()
 
-        self.button = tk.Button(self.filePathFrame, text="F", font=('Roboto', 16))
-        self.button.grid(row=0, column=1, sticky=tk.W + tk.E)
+        self.buildButton = ctk.CTkButton(
+            self,
+            text = 'Build',
+            command = self.build
+            )
+        self.buildButton.pack()
 
-        self.filePathFrame.pack(fill='x', padx=10)
+    def toggleColorMode(self):
+        global currentMode
+        if currentMode == True:
+            self.colorModeButton.configure(ctk.set_appearance_mode('dark'))
+            currentMode = False
+        
+        else:
+            self.colorModeButton.configure(ctk.set_appearance_mode('light'))
+            currentMode = True
 
-        self.window.mainloop()
+    def askDir(self):
+        global buildDir
+        buildDir = fg.askdirectory()
+        return
+
+    def build(self):
+        setBuildDir = sp.run(['powershell', 'cd', buildDir])
+        buildProject = sp.run(
+            ['powershell', 
+            r'C:\NVPACK\android-sdk-windows\platform-tools\adb install -r testbuild.apk']
+            )
+        return
+
+window = Main()
+window.mainloop()
