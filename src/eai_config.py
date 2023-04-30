0ed0f3a951d60bd3a4c716e04f3dc27a76db819c
Aryn Taylor <aryntaylor2021@gmail.com>
2023-04-30 15:46:45 +0100

    Change backslashes to forwardslashes for better portability

--- src/eai_config.py
+++ src/eai_config.py
@@ -1,8 +1,5 @@
 # This is the config file for eai_main.py
 
-
-# This is the config file for eai_main.py
-
 from PIL import Image, ImageTk
 import customtkinter as ctk
 
@@ -11,5 +8,5 @@ COLORMODE_IMAGE = ctk.CTkImage(dark_image=Image.open(r"assets/icon_cm.png"))
 FILEDIALOG_IMAGE = ctk.CTkImage(dark_image=Image.open(r"assets/icon_file.png"))
 
 ADB_PATH1 = r'adb install -r'
-ADB_PATH2 = r'C:\Users\%userprofile%\AppData\Local\Android\Sdk\platform-tools\adb.exe install -r'
-ADB_PATH3 = r'C:NVPACK\android-sdk-windows\platform-tools\adb.exe install -r'
+ADB_PATH2 = r'C:/Users/%userprofile%/AppData/Local/Android/Sdk/platform-tools/adb.exe install -r'
+ADB_PATH3 = r'C:/NVPACK/android-sdk-windows/platform-tools/adb.exe install -r'
